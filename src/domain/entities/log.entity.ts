90e8bdaff5bf0b25265b306646b5c9eb73aec865
itsdrc <anexomailer@gmail.com>
2024-05-13 13:32:12 -0500

    add: LogEntity

---
+++ src/domain/entities/log.entity.ts
@@ -0,0 +1,73 @@
+
+export enum LogEntityLevel {
+    low = 'low',
+    medium = 'medium',
+    high = 'high',
+}
+
+export interface Log {
+    message: string,
+    level: LogEntityLevel
+    origin: string,
+    createdAt?: Date
+}
+
+export class LogEntity {
+
+    public message: string;
+    public level: LogEntityLevel;
+    public origin: string;
+    public createdAt: Date;
+
+    constructor(log: Log) {
+
+        const { message, level, origin, createdAt = new Date() } = log;
+        this.message = message;
+        this.level = level;
+        this.origin = origin;
+        this.createdAt = createdAt;
+    }
+
+    static fromJson(jsonString: string): LogEntity {
+
+        if (!jsonString)
+            jsonString = '{}';
+
+        const { message, level, origin, createdAt } = JSON.parse(jsonString);
+
+        const log = new LogEntity({
+            message,
+            level,
+            origin,
+            createdAt,
+        })
+
+        return log;
+    }
+
+    static fromObject(object: { [key: string]: any }): LogEntity {
+
+        const { message, level, origin, createdAt } = object;
+
+        if (!message)
+            throw new Error('message property is missing');
+
+        if (!level)
+            throw new Error('level property is missing');
+
+        if (!origin)
+            throw new Error('origin property is missing');
+
+        // createdAt has a default value in LogEntity constructor
+
+        const log = new LogEntity({
+            message,
+            level,
+            origin,
+            createdAt
+        });
+
+        return log;
+    }
+
+}
