91dc247d3d25e4d9b8e2bd47a62372b7d828a4fc
itsdrc <anexomailer@gmail.com>
2024-05-13 16:36:02 -0500

    update: app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import { ENVS } from "./config/envs";
 import { MongoDatabase } from "./infraestructure/databases/mongo/init";
-
+import { MongoDataSource } from "./infraestructure/datasources/mongoDataSource";
+import { NOCServer } from "./presentation/server";
+import { CheckAndLog } from "./use-cases/check/check-and-log";
 
 (async () => {
-    await main();
+    await main().catch(error => console.error(error));
 })();
 
 async function main() {
-    
-    await MongoDatabase.connect(ENVS.MONGO_URL);
-    console.log('Connected');
 
-}
+    await MongoDatabase.connect(ENVS.MONGO_URL);
+    const mongods = new MongoDataSource();
+
+    const logger = new CheckAndLog(
+        mongods,
+        () => console.log('Connection is working'),
+        (error) => console.log('Connection is NOT working'),
+    );
+
+    NOCServer.start('*/5 * * * * *', async () => {
+        await logger.execute(ENVS.URL);
+    });
+
+}  
