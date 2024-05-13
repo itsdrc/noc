75ba7a590f7e8f5211bc7cb9c126695914abbb73
itsdrc <anexomailer@gmail.com>
2024-05-13 14:10:25 -0500

    fix: savelog

--- src/domain/abstract-classes/logDataSource.ts
+++ src/domain/abstract-classes/logDataSource.ts
@@ -3,6 +3,6 @@ import { LogEntity, LogEntityLevel } from "../entities/log.entity";
 
 export abstract class LogDataSource {
 
-    abstract saveLogs(log: LogEntity): Promise<void>;
+    abstract saveLog(log: LogEntity): Promise<void>;
     abstract getLogs(severityLevel: LogEntityLevel): Promise<LogEntity[]>;
 }
