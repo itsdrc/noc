3ebb7dbeaf0222d1acc5323e2ea5b1735c895f16
itsdrc <anexomailer@gmail.com>
2024-05-14 13:35:06 -0500

    add: envs test

--- testing/config/envs.test.ts
+++ testing/config/envs.test.ts
@@ -1,9 +1,11 @@
 
 describe('envs', () => {
 
-    test('testName', async () => {
+    test('envs test', async () => {
 
-       
+        expect(process.env.MONGO_URL).not.toBeUndefined();                                    
+        expect(process.env.URL).not.toBeUndefined();
+        expect(process.env.DISCORD_WEBHOOK_URL).not.toBeUndefined();
     });
 
 });
