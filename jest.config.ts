c9f22e85e709cdcc969a3c5c71e09675b847f730
Diego Rodriguez <anexomailer@gmail.com>
2024-05-18 10:17:53 -0500

    database test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
 
@@ -199,6 +199,12 @@ const config: Config = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+  "watchPathIgnorePatterns": [
+    '<rootDir>/mongo-test/',
+    '<rootDir>/mongo/',
+    '<rootDir>/postgres-test/',
+    '<rootDir>/postgres',
+  ]
 };
 
 export default config;
