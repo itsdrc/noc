c846d61bc898ff0e3b8e5bfd0823f2ae879edfa4
itsdrc <anexomailer@gmail.com>
2024-05-18 21:06:33 -0500

    fix: prisma should use lowercase severity levels

--- src/infraestructure/datasources/postgresDataSource.ts
+++ src/infraestructure/datasources/postgresDataSource.ts
@@ -3,30 +3,19 @@ import { LogDataSource } from "../../domain/abstract-classes/logDataSource";
 import { LogEntity, LogEntityLevel } from "../../domain/entities/log.entity";
 import { prisma } from "../databases/postgres/init";
 
-const severityEnum = {
-    low: SeverityLevel.LOW,
-    medium: SeverityLevel.MEDIUM,
-    high: SeverityLevel.HIGH
-}
-
 export class PostgreDataSource implements LogDataSource {
 
     async saveLog(log: LogEntity): Promise<void> {
         
-        const level = severityEnum[log.level];
         await prisma.prismaLogModel.create({
-            data: {
-                ...log,
-                level,
-            }
+            data: log
         });
     }
 
     async getLogs(severityLevel: LogEntityLevel): Promise<LogEntity[]> {
-        
-        const level = severityEnum[severityLevel];
+                
         const logs = await prisma.prismaLogModel.findMany({
-            where: {level},
+            where: {level: severityLevel},
         });
 
         return logs.map(LogEntity.fromObject);
